f03f935bf9ebdd425f8d38127b85ff57534251ae
Eliaris Alvares <eliarisalvares@Mac-mini-de-Eliaris.local>
2022-10-11 17:55:34 -0300

    hello world

---
+++ Python Studies/hello_word.py
@@ -0,0 +1,13 @@
+# Python uses dynamic typing, there is no need to declare the variable type.
+# Unlike C, which allows explicit type casting of the variables, Python
+# does not support explicit type casting and the variable can't be static, it
+# needs to be assigned a value to exist. Also, Python uses snake case naming.
+# such as: "my_variable_name" (all letters are lowercased).
+
+hello = "Hello"
+world = "World"		# Create two string variables
+
+print(hello, word, sep="_", end="!")	# Print the strings using "_"
+# as the separator and "!" as the finish character
+type(hello)			# Returns its type as <class 'str'>
+type(world)			# Returns its type as  <class 'str'>
